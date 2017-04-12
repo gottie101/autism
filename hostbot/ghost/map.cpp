f992d3b24a747bed8bfc085a8ed600636288613f
Mike <mike@ring0.cc>
2017-04-12 17:19:17 +0200

    revert MPQ flat

--- hostbot/ghost/map.cpp
+++ hostbot/ghost/map.cpp
@@ -272,7 +272,7 @@ void CMap :: Load( CConfig *CFG, string nCFGFile )
 	HANDLE MapMPQ;
 	bool MapMPQReady = false;
 
-	if( SFileOpenArchive( MapMPQFileName.c_str( ), 0, MPQ_OPEN_READ_ONLY, &MapMPQ ) )
+	if( SFileOpenArchive( MapMPQFileName.c_str( ), 0, MPQ_OPEN_FORCE_MPQ_V1, &MapMPQ ) )
 	{
 		CONSOLE_Print( "[MAP] loading MPQ file [" + MapMPQFileName + "]" );
 		MapMPQReady = true;
